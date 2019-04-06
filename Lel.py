426ea9723fdcc31c68fe0e69847db22b4fb189c4
Seb <47443826+InAdequated@users.noreply.github.com>
2019-04-07 02:09:44 +1000

    Random Insult Generator Source Code

---
+++ Lel.py
@@ -0,0 +1,77 @@
+import random
+def randominsgen():
+    r=random.randint(1,9)
+    r2=random.randint(1,9)
+    r3=random.randint(1,9)
+    in1="faggot"
+    in2="cunt"
+    in3="shithead"
+    in4="carrot"
+    in5="fat"
+    in6="orangatang"
+    in7="skinny"
+    in8="ass"
+    in9="bitch"
+    if r==1:
+        ins1="faggot"
+    elif r==2:
+        ins1="cunt"
+    elif r==3:
+        ins1="shithead"
+    elif r==4:
+        ins1="carrot"
+    elif r==5:
+        ins1="fat"
+    elif r==6:
+        ins1="orangatang"
+    elif r==7:
+        ins1="skinny"
+    elif r==8:
+        ins1="ass"
+    elif r==9:
+        ins1="bitch"
+
+    if r2==1:
+        ins2="faggot"
+    elif r2==2:
+        ins2="cunt"
+    elif r2==3:
+        ins2="shithead"
+    elif r2==4:
+        ins2="carrot"
+    elif r2==5:
+        ins2="fat"
+    elif r2==6:
+        ins2="orangatang"
+    elif r2==7:
+        ins2="skinny"
+    elif r2==8:
+        ins2="ass"
+    elif r2==9:
+        ins2="bitch"
+    
+    if r3==1:
+        ins3="faggot"
+    elif r3==2:
+        ins3="cunt"
+    elif r3==3:
+        ins3="shithead"
+    elif r3==4:
+        ins3="carrot"
+    elif r3==5:
+        ins3="fat"
+    elif r3==6:
+        ins3="orangatang"
+    elif r3==7:
+        ins3="skinny"
+    elif r3==8:
+        ins3="ass"
+    elif r3==9:
+        ins3="bitch"
+    print(ins1 + " " + ins2 + " " + ins3)
+
+
+while True:
+    randominsgen()
+    resp=str(input("Wha'cha gan say PUNK? "))
+    
